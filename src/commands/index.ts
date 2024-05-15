55a43308fc88cab834f6222db42ec475bfa003f1
Jomariel Gaitera <jojogaitera@gmail.com>
2024-05-16 00:23:56 +0800

    Optimize

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,13 @@
-export * from './ping';
-export * from './ignore-add';
-export * from './ignore-list';
-export * from './ignore-remove';
-export * from './disable';
-export * from './enable';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+async function autoload(): Promise<void> {
+  const files = await fs.readdir(__dirname);
+  const isCommand = (path: string) => /\.command\.(ts|js)$/i.test(path);
+  const commands = files.filter(isCommand);
+  const cleanImport = (file: string) => import(path.resolve(__dirname, file));
+
+  void Promise.all(commands.map(cleanImport));
+}
+
+void autoload();
