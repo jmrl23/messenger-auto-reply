dfafe6b39fba762001f45a07e631560fae723344
Jomariel Gaitera <jojogaitera@gmail.com>
2024-05-16 00:39:49 +0800

    Add commands

---
+++ src/commands/mode-list.command.ts
@@ -0,0 +1,18 @@
+import { MODE } from '../constants/data.constant';
+import CommandService from '../services/command.service';
+
+CommandService.command<CommandCallback>(
+  'mode list',
+  async function modeList(_, message) {
+    const keys = Object.keys(MODE).filter((value) => isNaN(Number(value)));
+    const keysStringified = keys.map((key) => `- ${key}`);
+
+    await message.reply(
+      { content: `modes:\n\n${keysStringified}` },
+      {
+        typing: false,
+        returnMessage: false,
+      },
+    );
+  },
+);
---
+++ src/commands/mode-set.command.ts
@@ -0,0 +1,38 @@
+import { MODE } from '../constants/data.constant';
+import CommandService from '../services/command.service';
+import DataService from '../services/data.service';
+
+const modes = new Map<string, MODE>([
+  ['ai', MODE.Ai],
+  ['busy', MODE.Busy],
+  ['offline', MODE.Offline],
+]);
+
+CommandService.command<CommandCallback>(
+  'mode set',
+  async function modeSet(_, message, ...args) {
+    const value = args.join(' ');
+    const mode = modes.get(value);
+
+    if (!mode) {
+      await message.reply(
+        { content: `invalid mode: ${value}` },
+        {
+          typing: false,
+          returnMessage: false,
+        },
+      );
+
+      return;
+    }
+
+    await DataService.set('mode', mode);
+    await message.reply(
+      { content: `mode: ${value}` },
+      {
+        typing: false,
+        returnMessage: false,
+      },
+    );
+  },
+);
